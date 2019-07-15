817e8f2a877183b4f1346cbe42e1c4973f372b14
WENGEM <emilio.wang@oocl.com>
2019-07-16 00:02:38 +0800

    complete parking lot

--- src/test/java/com/thoughtworks/tdd/ParkingBoyTest.java
+++ src/test/java/com/thoughtworks/tdd/ParkingBoyTest.java
@@ -1,202 +1,187 @@
 package com.thoughtworks.tdd;
 
+import com.thoughtworks.tdd.entity.Car;
+import com.thoughtworks.tdd.entity.Ticket;
+import com.thoughtworks.tdd.service.Customer;
+import com.thoughtworks.tdd.service.ParkingBoy;
+import com.thoughtworks.tdd.service.ParkingLot;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ParkingBoyTest {
-
     @Test
-    public void should_return_ticket_when_car_in_parking_lot() throws Exception {
-        //given
-        Car car = new Car("粤C 888888");
+    void should_return_car_when_give_ticket() throws Exception {
+
+        //Given
+        Car car = new Car();
+        ParkingLot parkingLot = new ParkingLot();
         ParkingBoy parkingBoy = new ParkingBoy();
-        //when
-        Ticket ticket = parkingBoy.takeMyTicket(car);
-        //then
-        Assertions.assertNotNull(ticket);
+        parkingBoy.addParkingLot(parkingLot);
+
+        //When
+        Ticket ticket = parkingBoy.parkCar(car);
+
+        //Then
+        assertEquals(car, parkingBoy.fetchCar(ticket));
     }
 
     @Test
-    public void should_return_car_when_given_ticket() throws Exception {
+    void should_return_cars_when_give_tickets() throws Exception {
+
+        //Given
+        Car car1 = new Car();
+        Car car2 = new Car();
+        ParkingLot parkingLot = new ParkingLot();
         ParkingBoy parkingBoy = new ParkingBoy();
-        Car parkingCar = new Car("粤C 888888");
-        Ticket ticket = parkingBoy.takeMyTicket(parkingCar);
-        Assertions.assertEquals(parkingCar, parkingBoy.takeMyCar(ticket));
+        parkingBoy.addParkingLot(parkingLot);
+
+        //When
+        Car fetchCar1 = parkingBoy.fetchCar(parkingBoy.parkCar(car1));
+        Car fetchCar2 = parkingBoy.fetchCar(parkingBoy.parkCar(car2));
+
+        //Then
+        assertEquals(car1, fetchCar1);
+        assertEquals(car2, fetchCar2);
     }
 
     @Test
-    public void should_return_my_car_when_given_ticket() throws Exception {
+    void should_return_exception_when_take_wrong_ticket() throws Exception {
+
+        //Given
+        Car car = new Car();
+        ParkingLot parkingLot = new ParkingLot();
         ParkingBoy parkingBoy = new ParkingBoy();
-        Car parkingCar = new Car("粤C 888888");
-        Ticket ticket = parkingBoy.takeMyTicket(parkingCar);
-        Assertions.assertEquals(parkingCar.getCarLicenseNumber(), parkingBoy.takeMyCar(ticket).getCarLicenseNumber());
-    }
+        parkingBoy.addParkingLot(parkingLot);
 
-    @Test
-    public void should_return_tickets_when_parking_boy_park_multiple_cars() throws Exception {
-        ParkingBoy parkingBoy = new ParkingBoy();
+        //When
+//        Ticket ticket = parkingBoy.parkCar(car);
+        Ticket ticket = new Ticket();
 
-        Car firstCar = new Car("粤C 111111");
-        Car secondCar = new Car("粤C 222222");
-        Ticket firstTicket = parkingBoy.takeMyTicket(firstCar);
-        Ticket secondTicket = parkingBoy.takeMyTicket(secondCar);
-
-        Assertions.assertEquals(firstTicket.getCarLicenseNumber(), firstCar.getCarLicenseNumber());
-        Assertions.assertEquals(secondTicket.getCarLicenseNumber(), secondCar.getCarLicenseNumber());
-    }
-
-    @Test
-    public void should_return_cars_when_given_ticket() throws Exception {
-        ParkingBoy parkingBoy = new ParkingBoy();
-
-        Car firstCar = new Car("粤C 111111");
-        Car secondCar = new Car("粤C 222222");
-        Ticket firstTicket = parkingBoy.takeMyTicket(firstCar);
-        Ticket secondTicket = parkingBoy.takeMyTicket(secondCar);
-
-        Assertions.assertEquals(firstCar, parkingBoy.takeMyCar(firstTicket));
-        Assertions.assertEquals(secondCar, parkingBoy.takeMyCar(secondTicket));
-    }
-
-    @Test
-    public void should_get_exception_when_give_wrong_ticket() throws Exception {
-        ParkingBoy parkingBoy = new ParkingBoy();
-
-        Car car = new Car("粤C 888888");
-        Ticket wrongTicket = new Ticket("粤C 888888");
-        parkingBoy.takeMyTicket(car);
-        Assertions.assertThrows(Exception.class, () -> parkingBoy.fetch(wrongTicket));
-    }
-
-    @Test
-    public void should_get_exception_when_dont_give_ticket() throws Exception {
-        ParkingBoy parkingBoy = new ParkingBoy();
-
-        Car car = new Car("粤C 888888");
-        Ticket nullTicket = new Ticket(null);
-        parkingBoy.takeMyTicket(car);
-        Assertions.assertThrows(Exception.class, () -> parkingBoy.fetch(nullTicket));
-    }
-
-    @Test
-    public void should_get_exception_when_give_ticket_has_been_used() throws Exception {
-        ParkingBoy parkingBoy = new ParkingBoy();
-
-        Car car = new Car("粤C 888888");
-        Ticket ticket = parkingBoy.takeMyTicket(car);
-        parkingBoy.fetch(ticket);
-        Assertions.assertThrows(Exception.class, () -> parkingBoy.fetch(ticket));
-    }
-
-    @Test
-    public void should_get_exception_when_fetch_car_and_parkingLot_is_not_full() {
-        ParkingBoy parkingBoy = new ParkingBoy();
-
-        int capacity = 10;
-        List<Ticket> tickets = new ArrayList<Ticket>();
-        for (int i = 0; i < capacity; i++) {
-            try {
-                Ticket ticket = parkingBoy.takeMyTicket(new Car("粤C 888888"));
-                tickets.add(ticket);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        Assertions.assertEquals(capacity, tickets.size());
-    }
-
-    @Test
-    public void should_get_exception_when_fetch_car_and_parkingLot_is_full() {
-        ParkingBoy parkingBoy = new ParkingBoy();
+        //Then
         Assertions.assertThrows(Exception.class, () -> {
-            for (int i = 0; i < 11; i++) {
-                parkingBoy.takeMyTicket(new Car("粤C 888888"));
-            }
+            parkingBoy.fetchCar(ticket);
         });
     }
 
     @Test
-    public void should_return_unrecognized_parking_ticket_when_give_wrong_ticket() throws Exception {
+    void should_fetch_no_car_when_not_give_ticket() throws Exception {
+
+        //Given
+        Car car = new Car();
+        ParkingLot parkingLot = new ParkingLot();
         ParkingBoy parkingBoy = new ParkingBoy();
-        String errorMsg = "Unrecognized parking ticket.";
-        try {
-            parkingBoy.takeMyCar(null);
-        } catch (Exception ex) {
-            Assertions.assertEquals(errorMsg, ex.getMessage());
-        }
+        parkingBoy.addParkingLot(parkingLot);
+
+        //When
+        parkingBoy.parkCar(car);
+
+        //Then
+        Assertions.assertThrows(Exception.class, () -> {
+            parkingBoy.fetchCar(null);
+        });
     }
 
     @Test
-    public void should_return_not_enough_position_when_fetch_car_and_parkingLot_is_full() {
+    void should_return_exception_when_parking_lot_has_no_position() throws Exception {
+
+        //Given
+        ParkingLot parkingLot = new ParkingLot();
         ParkingBoy parkingBoy = new ParkingBoy();
-        String errorMsg = "Not enough position.";
-        try {
-            for (int i = 0; i < 11; i++) {
-                parkingBoy.takeMyTicket(new Car("粤C 888888"));
-            }
-        } catch (Exception ex) {
-            Assertions.assertEquals(errorMsg, ex.getMessage());
+        parkingBoy.addParkingLot(parkingLot);
+
+        //When
+        for (int i = 0; i < 10; i++) {
+            Car car = new Car();
+            parkingBoy.parkCar(car);
         }
+        //Then
+        Assertions.assertThrows(Exception.class, () -> {
+            parkingBoy.parkCar(new Car());
+        });
     }
 
     @Test
-    public void should_return_park_in_parking_lot_2_when_parking_lot_1_is_full() throws Exception {
-        List<ParkingLot> parkingLots = new ArrayList<>();
-        Car car = new Car("粤C 888888");
-        ParkingLot parkingLot1 = new ParkingLot(0);
-        ParkingLot parkingLot2 = new ParkingLot();
-        parkingLots.add(parkingLot1);
-        parkingLots.add(parkingLot2);
-        ParkingBoy parkingBoy = new ParkingBoy(parkingLots);
-        parkingBoy.takeMyTicket(car);
+    void should_return_unrecognized_parking_ticket_when_give_wrong_ticket() throws Exception {
 
-        ParkingLot countOfCars = parkingBoy.getParkingLots().get(1);
+        //Given
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy();
+        parkingBoy.addParkingLot(parkingLot);
+        Customer customer = new Customer();
+        customer.setFetchCar(parkingBoy);
+        customer.setPark(parkingBoy);
 
-        Assertions.assertEquals(1, countOfCars);
+        //When
+        Ticket wrongTicket = new Ticket();
+        customer.setParkingTicket(wrongTicket);
+
+        //Then
+        assertEquals("Unrecognized parking ticket.", customer.fetchMyCar());
     }
 
     @Test
-    public void park_by_default_in_parking_lot_with_fewer_cars() throws Exception {
-        ParkingBoy parkingBoy = new SmartParkingBoy(2);
-        Car car = new Car("粤C 111111");
-        parkingBoy.takeMyTicket(car);
-        parkingBoy.takeMyTicket(car);
-        Assertions.assertEquals(parkingBoy.getParkingLots().get(0).getTicketCars().size()
-                , parkingBoy.getParkingLots().get(1).getTicketCars().size());
+    void should_return_please_provide_your_parking_ticket_when_null_ticket() throws Exception {
+        //Given
+
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy();
+        parkingBoy.addParkingLot(parkingLot);
+        Customer customer = new Customer();
+        customer.setFetchCar(parkingBoy);
+        customer.setPark(parkingBoy);
+
+        //When
+        String message = customer.fetchMyCar();
+
+        //Then
+        assertEquals("Please provide your parking ticket.", message);
     }
 
     @Test
-    public void park_by_default_in_parking_lot_with_low_position_rate() throws Exception {
-        ParkingBoy parkingBoy = new SuperSmartParkingBoy(2);
-        Car car = new Car("粤C 111111");
-        for (int i = 0; i < 6; i++) {
-            parkingBoy.takeMyTicket(car);
+    void should_return_not_enough_position_when_parking_lot_is_full() throws Exception {
+
+        //Given
+        Car eleventhCar = new Car();
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy();
+        parkingBoy.addParkingLot(parkingLot);
+        Customer customer = new Customer();
+        customer.setFetchCar(parkingBoy);
+        customer.setPark(parkingBoy);
+        customer.setCar(eleventhCar);
+
+        //When
+        for (int i = 0; i < 10; i++) {
+            Car car = new Car();
+            parkingBoy.parkCar(car);
         }
-        Assertions.assertEquals(3, parkingBoy.getParkingLots().get(0).getTicketCars().size());
-        Assertions.assertEquals(3, parkingBoy.getParkingLots().get(1).getTicketCars().size());
+
+        //Then
+        assertEquals("Not enough position.", customer.parkMyCar());
     }
 
     @Test
-    public void should_manager_add_parkingBoy() throws Exception {
-        Manager manager = new Manager();
-        for (int i = 0; i < 6; i++) {
-            manager.addParkingBoy(new ParkingBoy());
+    void should_return_new_car_in_no2_parking_lot_when_no1_parking_lot_is_full() throws Exception {
+
+        //Given
+        ParkingBoy parkingBoy = new ParkingBoy();
+        parkingBoy.addParkingLot(new ParkingLot());
+        parkingBoy.addParkingLot(new ParkingLot());
+        Customer customer = new Customer();
+        customer.setFetchCar(parkingBoy);
+        customer.setPark(parkingBoy);
+        customer.setCar(new Car());
+
+        //When
+        for (int i = 0; i < 10; i++) {
+            Car car = new Car();
+            parkingBoy.parkCar(car);
         }
-        Assertions.assertEquals(6, manager.getParkingBoyList().size());
-    }
+        Ticket ticket = parkingBoy.parkCar(new Car());
 
-    @Test
-    public void  should_get_car_by_manager()throws Exception{
-        Manager manager = new Manager();
-        Car car = new Car("粤C 111111");
-        Ticket ticket = manager.takeMyTicket(car);
-        Car carManager = manager.fetch(ticket);
-        Assertions.assertEquals(car,carManager);
+        //Then
+        assertEquals(customer.getCar(), parkingBoy.fetchCar(ticket));
     }
 }
-
-
